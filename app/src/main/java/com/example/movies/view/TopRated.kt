976fd68609486e62df86e2bf227df9263a7f36ba
RBianchi-Al <renatalebianchi@gmail.com>
2021-08-05 22:30:40 -0300

    'final'

---
+++ app/src/main/java/com/example/movies/view/TopRated.kt
@@ -0,0 +1,11 @@
+package com.example.movies.model
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TopRated : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_top_rated)
+    }
+}
